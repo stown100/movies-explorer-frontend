465469a0e812f5961562d463b194ce809f5d90f0
Сергей <sergei1234565@yandex.ru>
2022-08-18 18:49:26 +0300

    push -in main

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -38,7 +38,7 @@ export const login = (email, password) => {
     return fetch(`${BASE_URL}/signin`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify({ email, password })
     })
